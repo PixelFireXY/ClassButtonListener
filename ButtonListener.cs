423650b50e93c57bbf4515c1adaa210aba0c5413
Matthew <60183477+PixelFireXY@users.noreply.github.com>
2021-09-09 15:52:22 +0200

    Add files via upload

---
+++ ButtonListener.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public abstract class ButtonListener : MonoBehaviour
+{    
+    protected void Start()
+    {
+        GetComponent<Button>().onClick.AddListener(() => OnClickButton());      
+    }
+
+    protected abstract void OnClickButton();    
+}
