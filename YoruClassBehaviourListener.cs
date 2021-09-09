b3f7421909901cae774a626be9d064023d6e710c
Matthew <60183477+PixelFireXY@users.noreply.github.com>
2021-09-09 15:51:33 +0200

    Add files via upload

---
+++ YoruClassBehaviourListener.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+
+public class YoruClassBehaviourListener : ButtonListener
+{
+    [ListToPopup(typeof(YoruClassBehaviourListener), "methodsList")]
+    [SerializeField] private string currentElementSelected;
+
+    [HideInInspector]
+    public List<string> methodsList = new List<string>();
+
+    /******************** Mono ********************/
+
+    private void Awake()
+    {
+        UpdateDropdownList();
+    }
+    private void OnValidate()
+    {
+        UpdateDropdownList();
+    }
+
+    /******************** Inspector button ********************/
+
+    private void UpdateDropdownList()
+    {
+        methodsList.Clear();
+
+        MethodInfo[] methodInfos = typeof(YourClassBehaviour).GetMethods();
+
+        foreach (var method in methodInfos)
+        {
+            if (method.DeclaringType == typeof(YourClassBehaviour) &&
+                method.IsPublic &&
+                method.ToString().Contains("System.Action") == false)
+            {
+                methodsList.Add(method.Name);
+            }
+        }
+    }
+
+    /******************** Button listener ********************/
+
+    protected override void OnClickButton()
+    {
+        YourClassBehaviour.Instance.Invoke(currentElementSelected, 0f);
+    }
+}
---
+++ YourClassBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class YourClassBehaviour : MonoBehaviour
+{
+	public static YourClassBehaviour Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+            Destroy(this);
+        else
+            Instance = this;
+    }
+
+    public void MethodShowedOnDropdown() { }
+    public void AnotherMethodShowedOnDropdown() { }
+    private void MethodNotShowedOnDropdown() { }
+}
